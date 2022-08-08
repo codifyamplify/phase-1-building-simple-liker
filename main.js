d20d58fa5c0be4b45f3985c65f8f7589806e6f27
David Whitney <codifyamplify@gmail.com>
2022-08-07 21:18:55 -0400

    commit tree e4d8c95efaf02cf83240ebea3a8e25a0f8c5e16f

--- main.js
+++ main.js
@@ -3,7 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const articleHearts = document.querySelectorAll(".like-glyph")
 
+
+function likeCallback(e) {
+  const heart = e.target
+  mimicServerCall("fakeWebsite.com")
+  // If the server response is successful make some heart-like-button changes
+    .then(function() {
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = ''
+      }
+    })
+  // If the server response is unsuccessful, display error modal by removing hidden class on the body modal thing
+  // 
+    .catch(function (error) {
+      const modal = document.getElementById("modal")
+      modal.className = '';
+      modal.innerText = error;
+      setTimeout(() => modal.className = "hidden", 3000)
+    })
+}
+
+for(const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback)
+}
 
 
 
